2488652fb13633275ca4ac3312687e54fe1b08a0
oipk582-beep <oipk582@gmail.com>
2026-09-19 15:13:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const { Client, GatewayIntentBits } = require("discord.js");
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildPresences
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`บอทออนไลน์แล้ว: ${client.user.tag}`);
+});
+
+client.on("presenceUpdate", (oldPresence, newPresence) => {
+  if (!newPresence.guild) return;
+
+  const member = newPresence.member;
+  if (!member) return;
+
+  console.log(
+    `${member.user.username} สถานะ: ${newPresence.status}`
+  );
+});
+
+client.login(process.env.DISCORD_TOKEN);
